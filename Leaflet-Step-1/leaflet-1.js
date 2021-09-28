09b6c0e15285c330eda89c224d2de452d0f991d1
MikeBinetsky <82287582+MikeBinetsky@users.noreply.github.com>
2021-09-27 20:30:24 -0400

    Basic map created

---
+++ Leaflet-Step-1/leaflet-1.js
@@ -0,0 +1,41 @@
+const urlPath = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+d3.json(urlPath).then(function(data){
+    createFeatures(data.features)
+});
+
+function createFeatures(earthquakeData) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+      };
+    var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+    });
+    createImageBitmap(eathquakes);
+};
+
+function createMap(earthquakes) {
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      })
+    
+      var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      });
+
+    var baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+
+    var overlays = {
+        "Earthquakes": earthquakes
+    };
+    var myMap = L.map("map", {
+        center: [0, 0],
+        zoom: 0,
+        layers: [street, earthquakes]
+    });
+    L.control.layers(basemaps, overlays, {
+    }).addTo(myMap);
+};
